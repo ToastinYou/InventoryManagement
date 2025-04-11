3e1185f5fd87e3597b1715e3943769e903c0228a
ToastinYou <zhartt19@outlook.com>
2025-04-11 08:11:55 -0400

    Hash passwords

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -1,12 +1,18 @@
 ﻿using InventoryManagement.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace InventoryManagement.Services;
 
 public class AccountService(IDataService dataService, IHttpContextAccessor httpContextAccessor) : IAccountService
 {
+    private const int SALT_SIZE = 16; // 128 bit
+    private const int KEY_SIZE = 32; // 256 bit
+    private const int ITERATIONS = 10000;
+
     public User? ActiveUser
     {
         get
@@ -24,9 +30,9 @@ public class AccountService(IDataService dataService, IHttpContextAccessor httpC
     public async Task<bool> LoginUserAsync(string email, string password)
     {
         List<User> users = dataService.GetUsers();
-        User? user = users.FirstOrDefault(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase) && u.Password == password);
+        User? user = users.FirstOrDefault(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
 
-        if (user is not null)
+        if (user is not null && VerifyPassword(password, user.Password))
         {
             List<Claim> claims =
             [
@@ -51,6 +57,7 @@ public class AccountService(IDataService dataService, IHttpContextAccessor httpC
 
     public async Task RegisterUserAsync(User user)
     {
+        user.Password = HashPassword(user.Password);
         dataService.AddUser(user);
         await LoginUserAsync(user.Email, user.Password);
     }
@@ -58,5 +65,29 @@ public class AccountService(IDataService dataService, IHttpContextAccessor httpC
     public async Task LogoutUserAsync()
     {
         await httpContextAccessor.HttpContext!.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+
+    private static string HashPassword(string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(SALT_SIZE);
+        byte[] hash = KeyDerivation.Pbkdf2(password, salt, KeyDerivationPrf.HMACSHA256, ITERATIONS, KEY_SIZE);
+
+        return $"{Convert.ToBase64String(salt)}.{Convert.ToBase64String(hash)}";
+    }
+
+    private static bool VerifyPassword(string password, string hashedPassword)
+    {
+        string[] parts = hashedPassword.Split('.', 2);
+        if (parts.Length != 2)
+        {
+            return false;
+        }
+
+        byte[] salt = Convert.FromBase64String(parts[0]);
+        byte[] hash = Convert.FromBase64String(parts[1]);
+
+        byte[] hashToCompare = KeyDerivation.Pbkdf2(password, salt, KeyDerivationPrf.HMACSHA256, ITERATIONS, KEY_SIZE);
+
+        return hashToCompare.SequenceEqual(hash);
     }
 }
